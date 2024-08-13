8d86d5953a9997eabc7a4597bcf050d03403193a
gtw123 <39769164+gtw123@users.noreply.github.com>
2024-08-14 00:58:27 +0800

    Change MissingCenterAtomMoleculeBuilder to pre-calculate element order

--- OpusSolver/Solver/AtomGenerators/Output/Hex3/MissingCenterAtomMoleculeBuilder.cs
+++ OpusSolver/Solver/AtomGenerators/Output/Hex3/MissingCenterAtomMoleculeBuilder.cs
@@ -25,14 +25,38 @@ namespace OpusSolver.Solver.AtomGenerators.Output.Hex3
         public override HexRotation OutputRotation => m_outputRotation;
         public override bool RequiresRotationsBetweenOutputPositions => false;
 
-        private List<Element> m_elementBuildOrder = new();
+        private List<Element> m_elementBuildOrder;
         private HexRotation m_outputRotation;
+        private bool m_needsAllBonds;
 
         public MissingCenterAtomMoleculeBuilder(AssemblyArea assemblyArea, Molecule product)
             : base(assemblyArea, product)
         {
             m_centerAtomPosition = FindCenterPosition();
+
+            DetermineBuildOrder();
+
             GenerateInstructions();
+        }
+
+        private void DetermineBuildOrder()
+        {
+            // Start with the first atom that doesn't have a bond in a clockwise direction.
+            var atoms = Product.GetAdjacentAtoms(CenterAtomPosition);
+            var startDir = atoms.Keys.FirstOrDefault(dir => atoms[dir].Bonds[dir - HexRotation.R120] == BondType.None);
+            var orderedAtoms = atoms.EnumerateCounterclockwise(startFrom: startDir);
+
+            m_elementBuildOrder = orderedAtoms.Select(a => a.Value.Element).ToList();
+
+            // For convenience, the first two atoms are swapped
+            (m_elementBuildOrder[0], m_elementBuildOrder[1]) = (m_elementBuildOrder[1], m_elementBuildOrder[0]);
+
+            m_outputRotation = HexRotation.R180 - orderedAtoms.Last().Key;
+            m_needsAllBonds = atoms.Keys.All(dir => atoms[dir].Bonds[dir - HexRotation.R120] != BondType.None);
+            if (m_needsAllBonds)
+            {
+                m_outputRotation = m_outputRotation.Rotate60Clockwise();
+            }
         }
 
         public override IEnumerable<Element> GetElementsInBuildOrder()
@@ -61,46 +85,31 @@ namespace OpusSolver.Solver.AtomGenerators.Output.Hex3
 
         private void GenerateInstructions()
         {
-            var atoms = Product.GetAdjacentAtoms(CenterAtomPosition);
-
-            // Start with the first atom that doesn't have a bond in a clockwise direction.
-            var startDir = atoms.Keys.FirstOrDefault(dir => atoms[dir].Bonds[dir - HexRotation.R120] == BondType.None);
-            var orderedAtoms = atoms.EnumerateCounterclockwise(startFrom: startDir).ToList();
-
             // Move the assembly arm so the product doesn't collide with it while being assembled
             Writer.Write(AssemblyArea.AssemblyArm, Instruction.MovePositive);
 
-            // Move the first atom to the RHS of the bonder. For convenience, the first atoms are actually swapped.
+            // Move the first atom to the RHS of the bonder
             Writer.Write(AssemblyArea.HorizontalArm, [Instruction.Grab, Instruction.MoveNegative]);
-            m_elementBuildOrder.Add(orderedAtoms[1].Value.Element);
 
             // Grab the second atom and rotate it so the two atoms are now facing away from the bonder
             Writer.NewFragment();
             Writer.Write(AssemblyArea.HorizontalArm, [Instruction.PivotCounterclockwise, Instruction.PivotCounterclockwise, Instruction.Reset]);
-            m_elementBuildOrder.Add(orderedAtoms[0].Value.Element);
 
-            for (int index = 2; index < orderedAtoms.Count; index++)
+            int atomCount = Product.Atoms.Count();
+            for (int index = 2; index < atomCount; index++)
             {
-                var (dir, atom) = orderedAtoms[index];
-
                 Writer.NewFragment();
-                m_elementBuildOrder.Add(atom.Element);
 
-                if (index < atoms.Count - 1)
+                if (index < atomCount - 1)
                 {
                     Writer.WriteGrabResetAction(AssemblyArea.HorizontalArm, [Instruction.MoveNegative, Instruction.PivotClockwise]);
                 }
             }
 
-            m_outputRotation = HexRotation.R180 - orderedAtoms.Last().Key;
-
-            bool needsAllBonds = atoms.Keys.All(dir => atoms[dir].Bonds[dir - HexRotation.R120] != BondType.None);
-            if (needsAllBonds)
+            if (m_needsAllBonds)
             {
                 Writer.WriteGrabResetAction(AssemblyArea.HorizontalArm, [Instruction.MoveNegative, Instruction.PivotClockwise]);
                 Writer.AdjustTime(2); // Move past the reset
-
-                m_outputRotation = m_outputRotation.Rotate60Clockwise();
             }
 
             // Move the assembled molecule to the output area
