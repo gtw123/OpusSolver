12a3a5c2d71107cbb639fc3c498bc475a301cb60
gtw123 <39769164+gtw123@users.noreply.github.com>
2024-08-25 23:53:26 +0800

    Explicitly disable repeating molecules for now

--- OpusSolver/Solver/LowCost/Output/MoleculeAssemblerFactory.cs
+++ OpusSolver/Solver/LowCost/Output/MoleculeAssemblerFactory.cs
@@ -14,33 +14,34 @@ namespace OpusSolver.Solver.LowCost.Output
 
         public MoleculeAssemblerFactory(IEnumerable<Molecule> products)
         {
-            if (!products.Any(p => p.HasTriplex))
-            {
-                if (products.All(p => p.Size == 1))
-                {
-                    if (products.Count() <= MonoatomicAssembler.MaxProducts)
-                    {
-                        m_createAssembler = (parent, writer, armArea) => new MonoatomicAssembler(parent, writer, armArea, products);
-                    }
-                    else
-                    {
-                        throw new SolverException($"LowCost solver can't currently handle more than {MonoatomicAssembler.MaxProducts} monoatomic products.");
-                    }
-                }
-                else if (products.All(p => p.IsLinear)) // includes monoatomic products
-                {
-                    m_createAssembler = (parent, writer, armArea) => new LinearAssembler(parent, writer, armArea, products);
-                }
-                else
-                {
-                    throw new SolverException("LowCost solver can't currently handle products with more than one atom.");
-                }
-            }
-            else
+            if (products.Any(p => p.HasTriplex))
             {
                 throw new SolverException("LowCost solver can't currently handle products with triplex bonds.");
             }
-
+            else if (products.Any(p => p.HasRepeats))
+            {
+                throw new SolverException("LowCost solver can't currently handle products with repeats.");
+            }
+            else if (products.All(p => p.Size == 1))
+            {
+                if (products.Count() <= MonoatomicAssembler.MaxProducts)
+                {
+                    m_createAssembler = (parent, writer, armArea) => new MonoatomicAssembler(parent, writer, armArea, products);
+                }
+                else
+                {
+                    throw new SolverException($"LowCost solver can't currently handle more than {MonoatomicAssembler.MaxProducts} monoatomic products.");
+                }
+            }
+            else if (products.All(p => p.IsLinear)) // includes monoatomic products
+            {
+                m_createAssembler = (parent, writer, armArea) => new LinearAssembler(parent, writer, armArea, products);
+            }
+            else
+            {
+                throw new SolverException("LowCost solver can't currently handle products with more than one atom.");
+            }
+ 
             m_productElementOrders ??= products.ToDictionary(p => p.ID, p => p.GetAtomsInInputOrder().Select(a => a.Element));
         }
     }
