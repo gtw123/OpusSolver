37da97219caa5f045114d0acd5e667d8db1e4b43
gtw123 <39769164+gtw123@users.noreply.github.com>
2024-10-15 01:27:49 +0800

    Improve low-cost MetalPurifier so it can handle out of order metals

--- OpusSolver/Solver/LowCost/MetalPurifier.cs
+++ OpusSolver/Solver/LowCost/MetalPurifier.cs
@@ -19,7 +19,7 @@ namespace OpusSolver.Solver.LowCost
         }
 
         private readonly StorageLocation[] m_storageLocations;
-        private bool m_hasInputAtom = false;
+        private AtomCollection m_atomOnGlyphInput;
 
         private static readonly Transform2D Input1Transform = new Transform2D(new Vector2(-1, 1), HexRotation.R0);
         private static readonly Transform2D Input2Transform = new Transform2D(new Vector2(0, 0), HexRotation.R0);
@@ -46,47 +46,95 @@ namespace OpusSolver.Solver.LowCost
 
         public override void Consume(Element element, int id)
         {
-            var transform = m_hasInputAtom ? Input2Transform : Input1Transform;
-            ArmController.DropMoleculeAt(transform, this);
+            var sequence = m_sequences[id];
 
-            if (m_hasInputAtom)
+            if (element == sequence.LowestMetalUsed)
             {
+                if (m_atomOnGlyphInput == null)
+                {
+                    m_atomOnGlyphInput = ArmController.DropMoleculeAt(Input1Transform, this);
+                    return;
+                }
+
+                ArmController.DropMoleculeAt(Input2Transform, this);
+                m_atomOnGlyphInput = null;
+            }
+            else
+            {
+                int metalIndex = element - sequence.LowestMetalUsed - 1;
+                var storageLocation = m_storageLocations[metalIndex];
+                if (storageLocation.Atom == null)
+                {
+                    storageLocation.Atom = ArmController.DropMoleculeAt(storageLocation.Transform, this);
+                    return;
+                }
+
+                if (m_atomOnGlyphInput != null)
+                {
+                    // We want to combine two metals but there's already a lower metal on one of the glyph inputs.
+                    // Temporarily move this atom out the way, then temporarily stash it in the location where the
+                    // other metal is being stored.
+                    ArmController.DropMoleculeAt(Input2Transform, this);
+
+                    ArmController.SetMoleculeToGrab(m_atomOnGlyphInput);
+                    ArmController.DropMoleculeAt(OutputTransform, this);
+
+                    ArmController.SetMoleculeToGrab(storageLocation.Atom);
+                    ArmController.DropMoleculeAt(Input1Transform, this);
+
+                    ArmController.SetMoleculeToGrab(m_atomOnGlyphInput);
+                    ArmController.DropMoleculeAt(storageLocation.Transform, this);
+                }
+                else
+                {
+                    ArmController.DropMoleculeAt(Input2Transform, this);
+
+                    ArmController.SetMoleculeToGrab(storageLocation.Atom);
+                    ArmController.DropMoleculeAt(Input1Transform, this);
+                }
+
+                storageLocation.Atom = null;
+            }
+
+            GridState.UnregisterAtom(Input1Transform.Position, this);
+            GridState.UnregisterAtom(Input2Transform.Position, this);
+
+            var newMetal = element + 1;
+            GridState.RegisterAtom(OutputTransform.Position, newMetal, this);
+
+            while (newMetal != sequence.TargetMetal)
+            {
+                int metalIndex = newMetal - sequence.LowestMetalUsed - 1;
+                var storageLocation = m_storageLocations[metalIndex];
+                if (storageLocation.Atom == null)
+                {
+                    storageLocation.Atom = new AtomCollection(newMetal, OutputTransform, this);
+                    ArmController.SetMoleculeToGrab(storageLocation.Atom);
+                    ArmController.DropMoleculeAt(storageLocation.Transform, this);
+                    break;
+                }
+
+                ArmController.SetMoleculeToGrab(storageLocation.Atom);
+                ArmController.DropMoleculeAt(Input2Transform, this);
+
+                ArmController.SetMoleculeToGrab(new AtomCollection(newMetal, OutputTransform, this));
+                ArmController.DropMoleculeAt(Input1Transform, this);
+
                 GridState.UnregisterAtom(Input1Transform.Position, this);
                 GridState.UnregisterAtom(Input2Transform.Position, this);
 
-                var newMetal = element + 1;
+                newMetal++;
                 GridState.RegisterAtom(OutputTransform.Position, newMetal, this);
 
-                var sequence = m_sequences[id];
-                while (newMetal != sequence.TargetMetal)
-                {
-                    int metalIndex = newMetal - sequence.LowestMetalUsed - 1;
-                    var storageLocation = m_storageLocations[metalIndex];
-                    if (storageLocation.Atom == null)
-                    {
-                        storageLocation.Atom = new AtomCollection(newMetal, OutputTransform, this);
-                        ArmController.SetMoleculeToGrab(storageLocation.Atom);
-                        ArmController.DropMoleculeAt(storageLocation.Transform, this);
-                        break;
-                    }
-
-                    ArmController.SetMoleculeToGrab(storageLocation.Atom);
-                    ArmController.DropMoleculeAt(Input2Transform, this);
-
-                    ArmController.SetMoleculeToGrab(new AtomCollection(newMetal, OutputTransform, this));
-                    ArmController.DropMoleculeAt(Input1Transform, this);
-
-                    GridState.UnregisterAtom(Input1Transform.Position, this);
-                    GridState.UnregisterAtom(Input2Transform.Position, this);
-
-                    newMetal++;
-                    GridState.RegisterAtom(OutputTransform.Position, newMetal, this);
-
-                    storageLocation.Atom = null;
-                }
+                storageLocation.Atom = null;
             }
 
-            m_hasInputAtom = !m_hasInputAtom;
+            if (m_atomOnGlyphInput != null)
+            {
+                // Move the stash atom back onto the glyph input
+                ArmController.SetMoleculeToGrab(m_atomOnGlyphInput);
+                ArmController.DropMoleculeAt(Input1Transform, this);
+            }
         }
 
         public override void Generate(Element element, int id)
